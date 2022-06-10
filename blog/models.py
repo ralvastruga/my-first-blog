10220633404dc02a4f9e028c93e97bdf0782f78f
Raul A.Astruga <r.alvarez.astruga@gmail.com>
2022-06-10 03:17:51 +0200

    Actualizado models a version EN

--- blog/models.py
+++ blog/models.py
@@ -1,30 +1,20 @@
+from django.conf import settings
 from django.db import models
-
-# Create your models here.
 from django.utils import timezone
-from django.contrib.auth.models import User
+
 
 class Post(models.Model):
-    STATUS_CHOICES = (
-        ("borrador", "Borrador"),
-        ("publicado", "Publicado"),
-        )
-    titulo = models.CharField(max_length = 250)
-    slug = models.SlugField(max_length = 250,
-                            unique_for_date = "publicar")
-    autor = models.ForeignKey(User,
-                              on_delete=models.CASCADE,
-                              related_name="blog_posts")
-    body = models.TextField()
-    publicar = models.DateTimeField(default=timezone.now)
-    creado = models.DateTimeField(auto_now_add=True)
-    actualizado = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10,
-                              choices=STATUS_CHOICES,
-                              default ="borrador")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
 
-    class Meta:
-        ordering = ("-publicar",)
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
     def __str__(self):
-        return self.titulo
+        return self.title
