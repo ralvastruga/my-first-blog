e2477fa37afaf5d4e4d4ac08e5a27636f77adfe3
Raul A.Astruga <r.alvarez.astruga@gmail.com>
2022-06-10 04:25:55 +0200

    Correccion de error en Views

--- blog/views.py
+++ blog/views.py
@@ -19,7 +19,7 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, "blog/post _edit.html", {"form": form})
+    return render(request, "blog/post_edit.html", {"form": form})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
